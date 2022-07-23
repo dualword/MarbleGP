efa4af5c47e81ceacda19a15474c6485a71ee6eb
DustbinGames <ckeimel@gmx.de>
2022-07-23 13:59:12 +0200

    Fix end race issue when player has withdrawn from race

--- project/_source/source/gameclasses/CDynamicThread.cpp
+++ project/_source/source/gameclasses/CDynamicThread.cpp
@@ -928,7 +928,7 @@ namespace dustbin {
       irr::s32 l_iIndex = a_MarbleId - 10000;
 
       if (l_iIndex >= 0 && l_iIndex < 16 && m_aMarbles[l_iIndex] != nullptr) {
-        if (m_aMarbles[l_iIndex]->m_eState == CObjectMarble::enMarbleState::Finished) {
+        if (m_aMarbles[l_iIndex]->m_eState == CObjectMarble::enMarbleState::Finished || m_aMarbles[l_iIndex]->m_eState == CObjectMarble::enMarbleState::Withdrawn) {
           sendRacefinished(1, m_pOutputQueue);
         }
         else if (m_aMarbles[l_iIndex]->m_iWithdraw == -1) {
