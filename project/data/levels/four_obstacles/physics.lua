cc9cf6cb1ac2a347ed3c16683da43e5ecc0e545b
DustbinGames <ckeimel@gmx.de>
2023-01-21 19:00:20 +0100

    Add re-done track "Moving: Four Obstacles"

--- project/data/levels/four_obstacles/physics.lua
+++ project/data/levels/four_obstacles/physics.lua
@@ -1,62 +1,168 @@
 
 system:executeluascript("levels/four_obstacles/track_functions.lua")
 
-g_Actions = {
-  [1] = 0,
-  [2] = 0,  -- 23124, 23127, 23129
-  [3] = 0   -- 23133, 23136, 23138
+g_HammerInterval = 360
+g_HammerVelocity = 4
+
+g_Hammer     = 0
+g_HammerTime = 0
+
+g_SlapInterval = 180
+g_SlepValocity = 5
+
+g_SlapStep = 0
+g_SlapTime = 0
+
+g_GateInterval = 240
+g_GateVelocity = 75
+g_GateState    = 0
+g_GateStep     = 0
+
+g_BlockInterval = 120
+g_BlockVecocity = 150
+g_BlockState    = 1
+g_BlockStep     = 0
+
+g_BlockData = {
+  [ 1] = { x =  1.0, y = 0.0, z =  0.0 },
+  [ 2] = { x =  0.0, y = 0.0, z = -1.0 },
+  [ 3] = { x = -1.0, y = 0.0, z =  0.0 },
+  [ 4] = { x = -1.0, y = 0.0, z =  0.0 },
+  [ 5] = { x =  0.0, y = 0.0, z = -1.0 },
+  [ 6] = { x =  1.0, y = 0.0, z =  0.0 },
+  [ 7] = { x =  1.0, y = 0.0, z =  0.0 },
+  [ 8] = { x =  0.0, y = 0.0, z =  1.0 },
+  [ 9] = { x = -1.0, y = 0.0, z =  0.0 },
+  [10] = { x = -1.0, y = 0.0, z =  0.0 },
+  [11] = { x =  0.0, y = 0.0, z =  1.0 },
+  [12] = { x =  1.0, y = 0.0, z =  0.0 }
 }
 
+function checkSlappers()
+  local l_RotationSlapOne = physics:getrotation(23384)
+  local l_RotationSlapTwo = physics:getrotation(23387)
+  
+  if g_SlapStep == 0 or g_SlapStep >= 2 then
+    if l_RotationSlapOne["y"] >= 87 then
+      physics:startmotor(23384, 0, 50)
+      physics:setangularvel(23384, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+  else
+    if l_RotationSlapOne["y"] <= -87 then
+      physics:startmotor(23384, 0, 50)
+      physics:setangularvel(23384, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+  end
+
+  if g_SlapStep == 3 then
+    if l_RotationSlapTwo["y"] >= 87 then
+      physics:startmotor(23387, 0, 50)
+      physics:setangularvel(23387, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+  else
+    if l_RotationSlapTwo["y"] <= -87 then
+      physics:startmotor(23387, 0, 50)
+      physics:setangularvel(23387, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+  end
+end
+
 function onstep(a_stepno)
-  if getGateStep(a_stepno) == 0 then
-    if g_Actions[1] == 0 then
-      physics:startmotor(23092, 25, 150)
-      physics:startmotor(23121, 25, 150)
-      
-      g_Actions[1] = 1
-    else
-      physics:startmotor(23092, -25, 150)
-      physics:startmotor(23121, -25, 150)
-      
-      g_Actions[1] = 0
+  -- Handle Obstacle One: Hammer
+  local l_RotationHammer = physics:getrotation(23331)
+  
+  if g_Hammer == 0 then
+    if l_RotationHammer["x"] <= -90 then
+      physics:startmotor(23331, 0, 500)
+      physics:setangularvel(23331, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+    
+    if a_stepno - g_HammerTime > g_HammerInterval then
+      g_HammerTime = a_stepno
+      physics:startmotor(23331, -g_HammerVelocity, 500)
+      g_Hammer = 1
+    end
+  elseif g_Hammer == 1 then
+    if l_RotationHammer["x"] >= 90 then
+      physics:startmotor(23331, 0, 500)
+      physics:setangularvel(23331, { x = 0.0, y = 0.0, z = 0.0 })
+    end
+    
+    if a_stepno - g_HammerTime > g_HammerInterval then
+      g_HammerTime = a_stepno
+      physics:startmotor(23331, g_HammerVelocity, 500)
+      g_Hammer = 0
     end
   end
   
-  if getBlockStep(a_stepno, 1) == 0 then
-    if g_Actions[2] == 0 then
-      physics:startmotor(23124, -50, 150)
-      g_Actions[2] = 1
-    elseif g_Actions[2] == 1 then
-      physics:startmotor(23127, -50, 150)
-      g_Actions[2] = 2
-    elseif g_Actions[2] == 2 then
-      physics:startmotor(23129, -50, 150)
-      g_Actions[2] = 3
-    elseif g_Actions[2] == 3 then
-      physics:startmotor(23124, 50, 150)
-      g_Actions[2] = 4
-    elseif g_Actions[2] == 4 then
-      physics:startmotor(23127, 50, 150)
-      g_Actions[2] = 5
-    elseif g_Actions[2] == 5 then
-      physics:startmotor(23129, 50, 150)
-      g_Actions[2] = 0
+  -- Handle Obstacle Two: Double-Slap
+  checkSlappers()
+  
+  if g_SlapStep == 0 then
+    if a_stepno - g_SlapTime > g_SlapInterval then
+      g_SlapTime = a_stepno
+      physics:startmotor(23384, -g_SlepValocity, 500)
+      g_SlapStep = 1
+    end
+  elseif g_SlapStep == 1 then
+    if a_stepno - g_SlapTime > g_SlapInterval then
+      g_SlapTime = a_stepno
+      physics:startmotor(23384, g_SlepValocity, 500)
+      g_SlapStep = 2
+    end
+  elseif g_SlapStep == 2 then
+    if a_stepno - g_SlapTime > g_SlapInterval then
+      g_SlapTime = a_stepno
+      physics:startmotor(23387, -g_SlepValocity, 500)
+      g_SlapStep = 3
+    end
+  elseif g_SlapStep == 3 then
+    if a_stepno - g_SlapTime > g_SlapInterval then
+      g_SlapTime = a_stepno
+      physics:startmotor(23387, g_SlepValocity, 500)
+      g_SlapStep = 0
     end
   end
   
-  if getCrushStep(a_stepno, 1) == 0 then
-    if g_Actions[3] == 0 then
-      physics:startmotor(23133, -25, 750)
-      physics:startmotor(23138,  25, 750)
-      g_Actions[3] = 1
-    elseif g_Actions[3] == 1 then
-      physics:startmotor(23136, -25, 750)
-      physics:startmotor(23133,  25, 750)
-      g_Actions[3] = 2
-    elseif g_Actions[3] == 2 then
-      physics:startmotor(23138, -25, 750)
-      physics:startmotor(23136,  25, 750)
-      g_Actions[3] = 0
+  -- Handle Obstacle three: the Gate
+  if g_GateState == 0 then
+    if a_stepno - g_GateStep > g_GateInterval then
+      physics:sethistop(23420, 71.0)
+      physics:startmotor(23420, g_GateVelocity, 500)
+      g_GateStep = a_stepno
+      g_GateState = 1
+    end
+  elseif g_GateState == 1 then
+    if a_stepno - g_GateStep > g_GateInterval then
+      physics:setlostop(23420, 35.5)
+      physics:startmotor(23420, -g_GateVelocity, 500)
+      g_GateStep = a_stepno
+      g_GateState = 2
+    end
+  elseif g_GateState == 2 then
+    if a_stepno - g_GateStep > g_GateInterval then
+      physics:setlostop(23420, 0.0)
+      physics:startmotor(23420, -g_GateVelocity, 500)
+      g_GateStep = a_stepno
+      g_GateState = 3
+    end
+  elseif g_GateState == 3 then
+    if a_stepno - g_GateStep > g_GateInterval then
+      physics:sethistop(23420, 35.0)
+      physics:startmotor(23420, g_GateVelocity, 500)
+      g_GateStep = a_stepno
+      g_GateState = 0
+    end
+  end
+  
+  -- Handle obstacle four: the Blocker
+  if a_stepno - g_BlockStep > g_BlockInterval then
+    physics:setjointaxis(23473, g_BlockData[g_BlockState])
+    g_BlockStep = a_stepno
+    g_BlockState = g_BlockState + 1
+    
+    if g_BlockData[g_BlockState] == nil then
+      g_BlockState = 1
     end
   end
 end
--- project/data/levels/four_obstacles/scene.lua
+++ project/data/levels/four_obstacles/scene.lua
@@ -2,31 +2,7 @@ g_GateAction = 1
 
 system:executeluascript("levels/four_obstacles/track_functions.lua")
 
+g_Hammer = 2
+
 function onstep(a_stepno)
-  local l_Block = getBlockStep(a_stepno - 60, 3)
-  
-  scene:setposition(23287, { x = 0.0, y = -l_Block / 1440.0, z = 0.0 })
-  
-  local l_Crush = getCrushStep(a_stepno - 360, 3)
-  
-  scene:setposition(24287, { x = -l_Crush / 1080.0, y = 0.0, z = 0.0 })
-  
-  local l_Gate = getGateStep(a_stepno)
-  
-  if l_Gate == 0 then
-    if g_GateAction == 0 then
-      g_GateAction = 1
-    else
-      g_GateAction = 0
-    end
-  end
-  
-  local l_Angle = -180.0 * (l_Gate / 600)
-  
-  if g_GateAction == 1 then
-    l_Angle = 180.0 - l_Angle
-  end
-  
-  scene:setrotation(24284, { x = 0.0, y = 0.0, z = 180.0 - l_Angle })
-  scene:setrotation(24285, { x = 0.0, y = 0.0, z = l_Angle })
 end
