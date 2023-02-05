57901a7aafe8bdb3cdbe28ca1575a04b23f2492e
DustbinGames <ckeimel@gmx.de>
2023-02-05 17:50:39 +0100

    Update version, adjust zip file name when creating releases

--- project/script/releasecreator.bat
+++ project/script/releasecreator.bat
@@ -9,7 +9,7 @@ python.exe createDataFiles.py
 del ..\..\MarbleGP\MarbleGP_Windows.exe
 copy ..\MarbleGP_Windows\x64\Release\MarbleGP_Windows.exe ..\..\MarbleGP
 SignTool sign /f "%PFX_FILE%" /p "%password%" /t http://timestamp.digicert.com /fd SHA256 ..\..\MarbleGP\MarbleGP_Windows.exe
-7z.exe a -r -tzip ..\release\MarbleGP_Windows.zip ..\..\MarbleGP\*.*
+7z.exe a -r -tzip ..\release\MarbleGP.zip ..\..\MarbleGP\*.*
 candle.exe marblegp.wixproj
 light.exe -ext WixUIExtension -cultures:en-us  -b ..\..\MarbleGP -out ..\release\MarbleGP.msi MarbleGP.wixobj -b ..\installer -dWixUILicenseRtf=license.rtf
 del marblegp.wixobj
