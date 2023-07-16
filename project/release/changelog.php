960b45845ab5f1967143965dea3881be11eb2c45
DustbinGames <ckeimel@gmx.de>
2023-07-16 15:41:23 +0200

    Add the changelog for the homepage to the repo

---
+++ project/release/changelog.php
@@ -0,0 +1,50 @@
+<ul>
+  <li>
+    <b>2023-07-16 [1.1 Build 237]</br>
+    <ul>
+      <li>Fixed a bug with the auto-throttle feature being (in)active by random</li>
+      <li>Little re-design of the in-game standings screen</li>
+      <li>Track &quot;Flat: Oval&quot; redone</li>
+    </ul>
+  </li>
+  <li>
+    <b>2023-07-02 [1.1 Build 236]</b>
+    <ul>
+      <li>Added icons with the starting numbers and color codes of all marbles to all lists during a championship for easier identification of the marbles</li>
+      <li>Fixed an issue with the AI help which completely stops the marble before a jump if no speed limit is set</li>
+    </ul>
+  </li>
+  <li>
+    <b>2023-06-19 [1.1 Build 232]</b>
+    <ul>
+      <li>More than 400 random names are now available</li>
+      <li>Reduced the number of items shown during a race by the AI help (Steer Left, Steer Right, Go Faster / Slower)</li>
+    </ul>
+  </li>
+  <li>
+    <b>2023-06-10 [1.1 Build 231]</b>
+    <ul>
+      <li>a virtual keyboard was added to the Android version of MarbleGP, available in the &quot;Edit Profiles&quot; dialog</li>
+      <li>random names have been added when creating a new profile</li>
+      <li>a random texture generator was added to the edit or add profile dialog</li>
+      <li>16 names for AI controlled marbles have been added</li>
+      <li>a bug was fixed which prevented creating a new profile if all slots were filled, the last profile was deleted and a new one was added</li>dings dialog, as well as during a game pause</li>
+      <li>more options were added to the selection of the classes of the AI marbles (Marble3, Marble3 + 2, Marble2, Marble2 + GP, MarbleGP, All Classes</li>
+      <li>icons were added to identify the bots in the game. These icons are visible in the &quot;Fill Grid&quot; dialog, in the result menu and in the stan
+    </ul>
+  </li>
+  <li>
+    <b>2023-06-02 [1.1 Build 229]</b>
+    <ul>
+      <li>the tutorial was enhanced with more details on how to control the marble</li>
+      <li>an &quot;auto throttle&quot; option was added to the controller which means that the marble always accelerates unless the player hits the brake</li>
+    </ul>
+  </li>
+  <li>
+    <b>2023-05-28 [1.1 Build 226]</b>
+    <ul>
+      <li>an enhanced touch and gyroscope controller was implemented. This new controller does always accelerate the marble unless the player brakes</li>
+      <li>the championship result scene was updated so that it's visually easier to distinguish between the first three places</li>
+    </ul>
+  </li>
+</ul>
